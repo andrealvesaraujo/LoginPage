cb10bcd17b8b22f1b9c9de28f3ee4ff488caa993
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-10-12 18:15:14 -0300

    Add class in html

--- src/Login.js
+++ src/Login.js
@@ -9,35 +9,35 @@ export default class Login extends React.Component {
 
   render() {
     return (
-      <main>
-        <div>
+      <main className="content">
+        <div className="container-aboutUs">
           <img src={people} alt="Wallpaper of AboutUs page" />
         </div>
         <div>
-          <div>
+          <div className="container-title">
               <h3>Bem vindo de volta</h3>
               <h1>Faça login na sua conta</h1>
           </div>
           <div>
-            <form>
-              <label for="email">E-mail</label>
+            <form className="container-form">
+                <label for="email">E-mail</label>
                 <input type="email" id="email" name="email" placeholder="example@gmail.com"/>
                 <label for="password">Senha</label>
                 <input type="password" id="password" name="password"/>
-                <div>
+                <div className="container-rememberMe">
                   <input type="checkbox" id="rememberMe" name="rememberMe"  />
                   <label for="rememberMe">Lembre de mim</label>
                   <a href="#">Esqueceu sua senha?</a>
                 </div>
-                <div>
+                <div className="container-button">
                   <button type="submit">Entrar</button>
                 </div>
-                <div>
+                <div className="container-button-google">
                   <button> <img src={googleIcon} alt="Icon of Google" /> Ou faça login com o Google </button>
                 </div>
             </form>
           </div>
-          <div>
+          <div className="container-register">
             <span>  Não tem uma conta? <a href="#">Cadastre-se</a> </span>
           </div>
         </div>

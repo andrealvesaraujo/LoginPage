8befd2292db83b48004860dcc003b895db2269ba
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-10-22 23:15:04 -0300

    refactor: Input component to login and password cases

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,17 +4,32 @@ import Input from "./Input"
 import Button from "./Button"
 
 import googleIcon from  '../img/icon_Google.png';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-// import { faLock } from '@fortawesome/free-solid-svg-icons'
-// import { faEye } from '@fortawesome/free-solid-svg-icons'
-// import { faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faLock } from '@fortawesome/free-solid-svg-icons'
+import { faEye } from '@fortawesome/free-solid-svg-icons'
+import { faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 
 export default class Form extends Component {
+
     render() {
         return (
           <form className="container-form">
-              <Input />
+              <Input 
+                label="E-mail" 
+                type="email" 
+                id="email" 
+                placeholder="example@gmail.com" 
+                leftIcon = {faEnvelope}
+              />
+              <Input 
+                label="Senha" 
+                type="password" 
+                id="password" 
+                leftIcon = {faLock} 
+                rightShowIcon = {faEye} 
+                rightHiddenIcon = {faEyeSlash} 
+              />
               <div className="container-rememberMe">
                 <div className="container-checkbox">
                   <input type="checkbox" id="rememberMe" name="rememberMe"  />
--- src/components/Input.js
+++ src/components/Input.js
@@ -2,27 +2,44 @@ import React, { Component } from 'react'
 import './Input.scss'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import { faLock } from '@fortawesome/free-solid-svg-icons'
-import { faEye } from '@fortawesome/free-solid-svg-icons'
-import { faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 
 export default class Input extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+           type : this.props.type
+        }
+    }
+
+    handlerPasswordIcon() {
+        if(this.state.canSee){
+            this.setState({
+                ...this.state,
+                canSee: false,
+                type:'password'
+            })
+    
+        } else {
+    
+            this.setState({
+                ...this.state,
+                canSee: true,
+                type:'text'
+            })
+        }
+      }
+
     render() {
         return (
             <>
-                <div className="container-email">
-                    <label htmlFor="email">E-mail</label>
-                    <input type="email" id="email" name="email" placeholder="example@gmail.com"/>
-                    <FontAwesomeIcon className="icon-email" icon={faEnvelope} />
+                <div className={`container-${this.props.type}`}>
+                    <label htmlFor={this.props.id}>{this.props.label}</label>
+                    <input type={this.state.type} id={this.props.id} name={this.props.type} placeholder={this.props.placeholder}/>
+                    {this.props.leftIcon && (<FontAwesomeIcon className="icon-left" icon={this.props.leftIcon} />)}
+                    {this.props.rightShowIcon && this.props.rightHiddenIcon && (<FontAwesomeIcon className="icon-right" icon={this.state.canSee ? this.props.rightHiddenIcon : this.props.rightShowIcon} onClick={() => this.handlerPasswordIcon()} />)}
                 </div>
-                <div className="container-password">
-                    <label htmlFor="password">Senha</label>
-                    <input type="password" id="password" name="password"/>
-                    <FontAwesomeIcon className="icon-lock" icon={faLock} />
-                    <FontAwesomeIcon className="icon-show" icon={faEye} />
-                    {/* <FontAwesomeIcon className="icon-hidden" icon={faEyeSlash} /> */}
-                </div>
+
             </>
         )
     }
